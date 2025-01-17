2b1801d32af8de7a92c7e7235a1224648f410549
Jaden <jadennmuniz@gmail.com>
2025-01-16 19:41:21 -0500

    Project 1

--- J_M_project_01.py
+++ J_M_project_01.py
@@ -1,11 +1,22 @@
 def main():
     name = "Professor Nedd"
-    print("We want to know if you like programming!")
-    print()
-    print("Do you like programming {}?".format(name))
+    print("Python is a type of coding program, and it might be the easiest one of all.")
+    print("What is your name, please?")
     answer = input()
-    print("Great! You said {}?".format(answer))
-    print("Let’s start learning some Python today")
+    print("Great! Your name is {}.".format(answer))
+    print("What is the name of the college you are currently attending, please?")
+    college_name = input()
+    print("Great! You are currently attending {}.".format(college_name))
+    print("What is the name of the high school you have attended, please?")
+    high_school_name = input()
+    print("{} is a great high school!".format(high_school_name))
+
+
+    print("All responses")
+    print(answer)
+    print(college_name)
+    print(high_school_name)
+
 
 if __name__ == "__main__":
     main()
