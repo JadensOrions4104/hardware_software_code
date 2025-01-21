1a91cd42fc9d2d64c81ead2b24f8f39d49df9825
Jaden <jadennmuniz@gmail.com>
2025-01-21 12:51:06 -0500

    Lab 9

--- J_M_project_01.py
+++ J_M_project_01.py
@@ -10,13 +10,24 @@ def main():
     print("What is the name of the high school you have attended, please?")
     high_school_name = input()
     print("{} is a great high school!".format(high_school_name))
+    print("Which of the two institutions is more fun, please?")
+    better_institution = input()
+    print("I understand. High school tends to be easier, but you'll do good in college, too!".format(better_institution))
+    print("This class is part of the major called Information Technology. Please tell me why you picked this major.")
+    reason_for_major = input()
+    print("Good reason.".format(reason_for_major))
+    print("Do you like coding?")
+    yes_no_coding = input()
+    print("Perfect! Let's start learning some Python today.".format(yes_no_coding))
 
 
     print("All responses")
     print(answer)
     print(college_name)
     print(high_school_name)
-
+    print(better_institution)
+    print(reason_for_major)
+    print(yes_no_coding)
 
 if __name__ == "__main__":
     main()
