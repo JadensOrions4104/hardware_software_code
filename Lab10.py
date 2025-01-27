8940081d9a661f0e6d41af153c2348c76cec037f
Jaden <jadennmuniz@gmail.com>
2025-01-27 12:55:53 -0500

    Lab 10

---
+++ Lab10.py
@@ -0,0 +1,11 @@
+def main():
+    print("This program adds two numbers.")
+
+    num1 = input("Enter first number:")
+    num2 = input("Enter second number:")
+    total = int(num1) + int(num2)
+
+    print("{} + {} = {}".format(num1, num2, total))
+
+if __name__ == "__main__":
+    main()
