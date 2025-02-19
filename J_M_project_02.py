c91b446d95bac0e1b9e45fde1c8304d4f06a55f1
Jaden <jadennmuniz@gmail.com>
2025-02-19 14:04:21 -0500

    Project 2 Jaden Muniz Hardware & Software

--- J_M_project_02.py
+++ J_M_project_02.py
@@ -10,6 +10,7 @@ def conversation():
     count = 0
     while True:
         print("Hello. I am going to ask you a series of questions so I can get to know a little more about you, if you don't mind. You can always end the conversation anytime by typing 'exit'.")
+
         print("What is your name, please?")
         answer = input()
         if answer.lower() == " exit" or answer.lower() == "exit " or answer.lower() == "exit":
@@ -18,6 +19,7 @@ def conversation():
         else:
             count +=1
         print("Great! Your name is {}.".format(answer))
+
         print("What's your favorite hobby, please?")
         fav_hobby = input()
         if fav_hobby.lower() == " exit" or fav_hobby.lower() == "exit " or fav_hobby.lower() == "exit":
@@ -26,6 +28,7 @@ def conversation():
         else:
             count +=1
         print("Great! Your favorite hobby is {}.".format(fav_hobby))
+
         print("What's your zodiac sign, please?")
         zodiac_sign = input()
         if zodiac_sign.lower() == " exit" or zodiac_sign.lower() == "exit " or zodiac_sign.lower() == "exit":
@@ -34,6 +37,7 @@ def conversation():
         else:
             count +=1
         print("{} sounds awesome!".format(zodiac_sign))
+
         print("What's your lucky number, please?")
         lucky_number = input()
         if lucky_number.lower() == " exit" or lucky_number.lower() == "exit " or lucky_number.lower() == "exit":
@@ -42,6 +46,7 @@ def conversation():
         else:
             count +=1
         print("Wow! That is lucky!".format(lucky_number))
+
         print("What's your favorite animal, please?")
         fav_animal = input()
         if fav_animal.lower() == " exit" or fav_animal.lower() == "exit " or fav_animal.lower() == "exit":
@@ -50,6 +55,22 @@ def conversation():
         else:
             count +=1
         print("I love that!".format(fav_animal))
+        print("What's your favorite color, please?")
+        fav_color = input()
+        if fav_color.lower() == " exit" or fav_color.lower() == "exit " or fav_color.lower() == "exit":
+            closing_statement(count)
+            break
+        else:
+            count +=1
+        print("Fantastic color!".format(fav_color))
+        print("Where's your favorite place to go, please?")
+        fav_place = input()
+        if fav_place.lower() == " exit" or fav_place.lower() == "exit " or fav_place.lower() == "exit":
+            closing_statement(count)
+            break
+        else:
+            count +=1
+        print("What a lovely place!".format(fav_place))
         print("Do you have any special talents, yes or no, please?")
         yes_no_talents = input().lower().strip()
         if yes_no_talents.lower() == " exit" or yes_no_talents.lower() == "exit " or yes_no_talents.lower() == "exit":
@@ -71,13 +92,14 @@ def main():
     # counter = 10
     # while_loop(counter)
     # for_loop(counter)
-    #
     # print("All responses")
     # print(answer)
     # print(fav_hobby)
     # print(zodiac_sign)
     # print(lucky_number)
     # print(fav_animal)
+    # print(fav_color)
+    # print(fav_place)
     # print(yes_no_talents)
 
 if __name__ == "__main__":
